2992499ec1039ce7747306fb65171f85e0837d25
Iason Boidanis <jjboi00113@gmail.com>
2023-05-07 20:21:52 +0300

    face a day video done

--- app/src/main/java/com/unipi/boidanis/firebasetest1/FaceFragment.java
+++ app/src/main/java/com/unipi/boidanis/firebasetest1/FaceFragment.java
@@ -8,10 +8,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaMuxer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,7 +24,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -32,13 +32,16 @@ import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.Spinner;
-import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -49,15 +52,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,7 +65,7 @@ import java.util.Objects;
  * create an instance of this fragment.
  */
 public class FaceFragment extends Fragment {
-    VideoView videoView;
+    ImageView imageView;
     String babyname;
     FirebaseDatabase database;
     DatabaseReference ref;
@@ -76,8 +76,12 @@ public class FaceFragment extends Fragment {
     FacePicture facePicture;
     ArrayList<FacePicture>list;
     ArrayList<String>facesDayList;
-    List<String> imageUris;
+    List<Uri> imageUris;
     List<Bitmap> bitmaps;
+    private int currentImageIndex = 0;
+    private Handler handler;
+    private Runnable updateImageRunnable;
+    private MediaController mediaController;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -123,11 +127,12 @@ public class FaceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_face, container, false);
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        /*if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1234);
-        }
+        }*/
 
-        videoView = view.findViewById(R.id.videoView);
+        imageView = view.findViewById(R.id.imageview11);
+
         Spinner getbabyname = (Spinner) getActivity().findViewById(R.id.spinner);
         babyname = getbabyname.getSelectedItem().toString();
 
@@ -154,11 +159,10 @@ public class FaceFragment extends Fragment {
                     facePicture = dataSnapshot.getValue(FacePicture.class);
                     list.add(facePicture);
                     facesDayList.add(String.valueOf( facePicture.getDay()));
-                    imageUris.add(facePicture.getImageUrl());
+                    imageUris.add(Uri.parse( facePicture.getImageUrl()));
                 }
                 faceADayAdapter.notifyDataSetChanged();
-                if(!imageUris.isEmpty()){
-                createVideoFromImages(imageUris);}
+
 
             }
 
@@ -187,127 +191,41 @@ public class FaceFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                videoView.setVideoURI(getOutputMediaFileUri());
-                videoView.start();
+                handler = new Handler();
+                updateImageRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        // Update the ImageView with the next image
+                        Glide.with(getContext()).load(imageUris.get(currentImageIndex))
+                                //.apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).centerCrop())
+                                .transition(DrawableTransitionOptions.withCrossFade())
+                                .into(imageView);
+                        // Increment the currentImageIndex
+                        currentImageIndex = (currentImageIndex + 1) % imageUris.size();
+                        if (currentImageIndex == 0) {
+                            // Remove the callback to stop the execution
+                            handler.removeCallbacks(updateImageRunnable);
+                            button2.setEnabled(true);
+                        } else {
+                            // Post the updateImageRunnable to run again in 5 seconds
+                            handler.postDelayed(updateImageRunnable, 5000);
+                        }
+                    }
+                };
+                handler.postDelayed(updateImageRunnable, 0); // Start the first update after 30 seconds
+                button2.setEnabled(false);
+
             }
         });
 
         return view;
     }
-    private void createVideoFromImages(List<String> imageUris) {
-        try {
-
-            MediaCodec codec = MediaCodec.createEncoderByType("video/avc");
-
-            MediaFormat format = MediaFormat.createVideoFormat("video/avc", 480, 640);
-
-            format.setInteger(MediaFormat.KEY_BIT_RATE, 200000);
-
-
-            format.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
-
-
-            format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
-
-
-            format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5);
-
-
-            codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-
-
-            Surface surface = codec.createInputSurface();
-
-
-            codec.start();
-
-            MediaMuxer muxer = new MediaMuxer(String.valueOf(getOutputMediaFileUri()), MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
-            showMessage("10","");
-
-            // Load images using Glide and convert them to bitmaps
-            List<Bitmap> bitmaps = new ArrayList<>();
-            showMessage("11","");
-
-            for (String uri : imageUris) {
-                showMessage("12","");
-
-                Bitmap bitmap = Glide.with(getContext()).asBitmap().load(uri).submit().get();
-                showMessage("13","");
-
-                bitmaps.add(bitmap);
-            }
-
-            // Encode bitmaps to video frames
-            for (int i = 0; i < bitmaps.size(); i++) {
-                Bitmap bitmap = bitmaps.get(i);
-                if (bitmap != null) {
-                    Canvas canvas = surface.lockCanvas(null);
-                    canvas.drawBitmap(bitmap, 0, 0, null);
-                    surface.unlockCanvasAndPost(canvas);
-                    codec.queueInputBuffer(codec.dequeueInputBuffer(-1), 0, 1, i * 1000000 / 30, 0);
-                    MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
-                    int outputBufferIndex = codec.dequeueOutputBuffer(info, 0);
-                    while (outputBufferIndex >= 0) {
-                        ByteBuffer outputBuffer = codec.getOutputBuffer(outputBufferIndex);
-                        muxer.writeSampleData(0, outputBuffer, info);
-                        codec.releaseOutputBuffer(outputBufferIndex, false);
-                        outputBufferIndex = codec.dequeueOutputBuffer(info, 0);
-                    }
-                }
-            }
-
-            // Release resources
-            codec.stop();
-            codec.release();
-            surface.release();
-            muxer.stop();
-            muxer.release();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onDestroy() {
+        // Remove the updateImageRunnable callbacks when the activity is destroyed to prevent memory leaks
+        handler.removeCallbacks(updateImageRunnable);
+        super.onDestroy();
     }
-    /*private String getOutputPath() {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String outputFileName = "VIDEO_" + timeStamp + ".mp4";
-        return getActivity().getExternalFilesDir(null) + "/" + outputFileName;
-    }*/
-    private Uri getOutputMediaFileUri() {
-        Uri videoUri = null;
-        try {
-
-
-            // Create a media file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String mediaFileName = "VID_" + timeStamp + ".mp4";
-
-            // Get the directory for storing the video
-            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), "MyAppVideos");
-
-            // Create the directory if it doesn't exist
-            if (!mediaStorageDir.exists()) {
-                if (!mediaStorageDir.mkdirs()) {
-                    Log.d(TAG, "Failed to create directory");
-                    return null;
-                }
-            }
-
-            // Create the file object
-            File mediaFile = new File(mediaStorageDir.getPath() + File.separator + mediaFileName);
-
-            // Save the video to the gallery
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Video.Media.TITLE, mediaFileName);
-            values.put(MediaStore.Video.Media.DESCRIPTION, "My video description");
-            values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-            values.put(MediaStore.Video.Media.DATA, mediaFile.getAbsolutePath());
-            videoUri = requireContext().getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
-            showMessage(videoUri.toString(), "");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return videoUri;
-    }
-
 
     void showMessage(String title, String message) {
         new AlertDialog.Builder(getActivity()).setTitle(title).setMessage(message).setCancelable(true).show();
