0ddbc277718cf3c785002b59f167c45552139fd0
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-05-22 19:55:43 +0300

    fixed speed on some parts

--- app/src/main/java/com/unipi/boidanis/firebasetest1/MainActivity6.java
+++ app/src/main/java/com/unipi/boidanis/firebasetest1/MainActivity6.java
@@ -24,8 +24,11 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -36,12 +39,13 @@ public class MainActivity6 extends AppCompatActivity {
     FirebaseUser user;
     FirebaseDatabase database;
     TextView textview;
-    DatabaseReference reference;
+    DatabaseReference reference,ref;
     ImageView imageview;
     private Uri imageUri;
     StorageReference storreference = FirebaseStorage.getInstance().getReference();
     private final int GALLERY_REQ_CODE = 1000;
     ProgressBar progressBar;
+    boolean check;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,42 +70,63 @@ public class MainActivity6 extends AppCompatActivity {
                 someActivityResultLauncher.launch(galleryIntent);
             }
         });
+        ref=database.getReference("Users").child(user.getUid()).child("parent info and settings");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    ParentInfo parentInfo = dataSnapshot.getValue(ParentInfo.class);
+                    if(parentInfo!=null){
+                        check=true;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         Button button = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(imageUri != null) {
-                    StorageReference fileRef = storreference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
-                    fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    ParentInfo parentInfo = new ParentInfo(uri.toString());
-                                    String key1 = reference.push().getKey();
-                                    reference.child("parent info and settings").child(key1).setValue(parentInfo);
-                                    progressBar.setVisibility(View.INVISIBLE);
-                                    imageview.setImageResource(R.drawable.ic_baseline_add_photo_alternate_24);
-                                    finish();
-                                    //Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
-                                    //startActivity(intent);
-                                }
-                            });
-                        }
-                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                            progressBar.setVisibility(View.VISIBLE);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressBar.setVisibility(View.INVISIBLE);
-                        }
-                    });
+                if(!check) {
+                    if (imageUri != null) {
+                        StorageReference fileRef = storreference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+                        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        ParentInfo parentInfo = new ParentInfo(uri.toString());
+                                        String key1 = reference.push().getKey();
+                                        reference.child("parent info and settings").child(key1).setValue(parentInfo);
+                                        progressBar.setVisibility(View.INVISIBLE);
+                                        imageview.setImageResource(R.drawable.ic_baseline_add_photo_alternate_24);
+                                        finish();
+                                        //Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                                        //startActivity(intent);
+                                    }
+                                });
+                            }
+                        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                                progressBar.setVisibility(View.VISIBLE);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                    } else {
+                        Toast.makeText(MainActivity6.this, "Please select picture", Toast.LENGTH_SHORT).show();
+                    }
                 }else{
-                    Toast.makeText(MainActivity6.this, "Please select picture", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity6.this, "Already selected profile picture", Toast.LENGTH_SHORT).show();
                 }
             }
         });
